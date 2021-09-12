63130986ede8a3c074547f4ffaa2a4cad814e277
taylor herbots <therbots@live.com>
2021-09-12 10:40:01 -0500

    added css to superheroTable to make a psuedo table

--- src/components/SuperheroTable/SuperheroTable.jsx
+++ src/components/SuperheroTable/SuperheroTable.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import './SuperHeroTable.css'
 
 const SuperheroTable = (props) => {
     return (
         <table>
-            <th>Name Primary Ability Secondary Ability</th>
-            {props.superhero.map((item =>
-                <tr><td key={item.superheroId}>{item.name} {item.primaryAbility} {item.secondaryAbility}</td></tr>
-                ))}
+            <thread>
+                <tr>
+                    <th>Name</th>
+                    <th>Primary Ability</th>
+                    <th>Secondary Ability</th>
+                </tr>
+            </thread>
+            <thread>
+                    {props.superhero.map((item =>
+                        <tr>
+                            <td key={item.superheroId}>{item.name}</td>
+                            <td>{item.primaryAbility}</td>
+                            <td>{item.secondaryAbility}</td>
+                        </tr>
+                        ))}
+            </thread>
         </table>
     );
 }
